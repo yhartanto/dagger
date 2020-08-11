6300d0277c958b0da48495482d5a66b73910456a
danysantiago <danysantiago@google.com>
2020-08-11 11:04:27 -0700

    Support an onCleared callback in the ActivityRetainedComponent.
    This adds a default binding to the ActivityRetainedComponent
    allowing users to register onCleared listeners that will be
    invoked when the activity retained component and its instances
    will be destroyed.
    
    RELNOTES=Add support for receiving a callback for when the activity retained component will no longer be used and destroyed.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=326057949

--- java/dagger/hilt/android/internal/managers/ActivityRetainedComponentManager.java
+++ java/dagger/hilt/android/internal/managers/ActivityRetainedComponentManager.java
@@ -21,22 +21,38 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.activity.ComponentActivity;
+import dagger.Binds;
+import dagger.Module;
 import dagger.hilt.EntryPoint;
 import dagger.hilt.EntryPoints;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.ActivityRetainedLifecycle;
 import dagger.hilt.android.components.ActivityRetainedComponent;
 import dagger.hilt.android.components.ApplicationComponent;
+import dagger.hilt.android.internal.ThreadUtil;
 import dagger.hilt.android.internal.builders.ActivityRetainedComponentBuilder;
+import dagger.hilt.android.scopes.ActivityRetainedScoped;
 import dagger.hilt.internal.GeneratedComponentManager;
+import java.util.HashSet;
+import java.util.Set;
+import javax.inject.Inject;
 
 /** A manager for the creation of components that survives activity configuration changes. */
 final class ActivityRetainedComponentManager
     implements GeneratedComponentManager<ActivityRetainedComponent> {
 
+  /** Entry point for {@link ActivityRetainedComponentBuilder}. */
   @EntryPoint
   @InstallIn(ApplicationComponent.class)
-  public interface LifecycleComponentBuilderEntryPoint {
+  public interface ActivityRetainedComponentBuilderEntryPoint {
     ActivityRetainedComponentBuilder retainedComponentBuilder();
+  }
+
+  /** Entry point for {@link Lifecycle}. */
+  @EntryPoint
+  @InstallIn(ActivityRetainedComponent.class)
+  public interface ActivityRetainedLifecycleEntryPoint {
+    ActivityRetainedLifecycle getActivityRetainedLifecycle();
   }
 
   static final class ActivityRetainedComponentViewModel extends ViewModel {
@@ -48,6 +64,15 @@ final class ActivityRetainedComponentManager
 
     ActivityRetainedComponent getComponent() {
       return component;
+    }
+
+    @Override
+    protected void onCleared() {
+      super.onCleared();
+      ActivityRetainedLifecycle lifecycle =
+          EntryPoints.get(component, ActivityRetainedLifecycleEntryPoint.class)
+              .getActivityRetainedLifecycle();
+      ((ActivityRetainedComponentManager.Lifecycle) lifecycle).dispatchOnCleared();
     }
   }
 
@@ -67,7 +92,8 @@ final class ActivityRetainedComponentManager
               public <T extends ViewModel> T create(@NonNull Class<T> aClass) {
                 ActivityRetainedComponent component =
                     EntryPoints.get(
-                            activity.getApplication(), LifecycleComponentBuilderEntryPoint.class)
+                            activity.getApplication(),
+                            ActivityRetainedComponentBuilderEntryPoint.class)
                         .retainedComponentBuilder()
                         .build();
                 return (T) new ActivityRetainedComponentViewModel(component);
@@ -91,4 +117,38 @@ final class ActivityRetainedComponentManager
     return viewModelProvider.get(ActivityRetainedComponentViewModel.class).getComponent();
   }
 
+  /** The default implementation of {@link ActivityRetainedLifecycle}. */
+  @ActivityRetainedScoped
+  static final class Lifecycle implements ActivityRetainedLifecycle {
+
+    private final Set<OnClearedListener> listeners = new HashSet<>();
+
+    @Inject
+    Lifecycle() {}
+
+    @Override
+    public void addOnClearedListener(@NonNull OnClearedListener listener) {
+      ThreadUtil.ensureMainThread();
+      listeners.add(listener);
+    }
+
+    @Override
+    public void removeOnClearedListener(@NonNull OnClearedListener listener) {
+      ThreadUtil.ensureMainThread();
+      listeners.remove(listener);
+    }
+
+    void dispatchOnCleared() {
+      for (OnClearedListener listener : listeners) {
+        listener.onCleared();
+      }
+    }
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  abstract static class LifecycleModule {
+    @Binds
+    abstract ActivityRetainedLifecycle bind(Lifecycle impl);
+  }
 }
