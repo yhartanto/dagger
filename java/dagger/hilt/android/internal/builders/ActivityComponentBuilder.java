0929fec4294bd43b3be966405d20375524d50f75
Dagger Team <java-team-github-bot@google.com>
2021-01-05 12:14:02 -0800

    Internal refactor
    RELNOTES=n/a
    PiperOrigin-RevId: 350193163

--- java/dagger/hilt/android/internal/builders/ActivityComponentBuilder.java
+++ java/dagger/hilt/android/internal/builders/ActivityComponentBuilder.java
@@ -24,6 +24,9 @@ import dagger.hilt.android.components.ActivityComponent;
 /** Interface for creating an {@link ActivityComponent}. */
 @DefineComponent.Builder
 public interface ActivityComponentBuilder {
-  ActivityComponentBuilder activity(@BindsInstance Activity activity);
+  ActivityComponentBuilder activity(
+      @BindsInstance
+          Activity activity);
+
   ActivityComponent build();
 }
