605e88c64b68cb6cddba8063dbfe50a2fdea3923
Brad Corso <bcorso@google.com>
2022-01-04 14:15:20 -0800

    Add Gradle test that repros issues in https://github.com/google/dagger/issues/3090.
    This CL adds a repro test for the issues in #3090 when a transitive annotation is no longer on the classpath.
    
    Follow-up CLs will fix the remaining issues.
    
    RELNOTES=N/A
    PiperOrigin-RevId: 419677447

---
+++ javatests/artifacts/dagger/transitive-annotation-app/library1/src/main/java/library1/Foo.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2021 The Dagger Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package library1;
+
+import static library2.MyTransitiveAnnotation.VALUE;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import library2.MyTransitiveAnnotation;
+
+/**
+ * A class used to test that Dagger won't fail when non-dagger related annotations cannot be
+ * resolved.
+ *
+ * <p>During the compilation of {@code :app}, {@link MyTransitiveAnnotation} will no longer be on
+ * the classpath. In most cases, Dagger shouldn't care that the annotation isn't on the classpath
+ */
+@Singleton
+// @MyTransitiveAnnotation(VALUE): Not supported on inject-types yet.
+public final class Foo extends FooBase {
+  @MyTransitiveAnnotation(VALUE)
+  int nonDaggerField;
+
+  // @MyTransitiveAnnotation(VALUE): Not supported on inject-fields yet.
+  @Inject int daggerField;
+
+  @MyTransitiveAnnotation(VALUE)
+  Foo(@MyTransitiveAnnotation(VALUE) String str) {
+    super(str);
+  }
+
+  // @MyTransitiveAnnotation(VALUE): Not supported on inject-constructors yet.
+  @Inject
+  Foo(
+      // @MyTransitiveAnnotation(VALUE): Not supported on inject-constructor parameters yet.
+      int i) {
+    super(i);
+  }
+
+  @MyTransitiveAnnotation(VALUE)
+  void nonDaggerMethod(@MyTransitiveAnnotation(VALUE) int i) {}
+
+  // @MyTransitiveAnnotation(VALUE): Not supported on inject-method yet.
+  @Inject
+  void daggerMethod(
+      // @MyTransitiveAnnotation(VALUE): Not supported on inject-method parameters yet.
+      int i) {}
+}
---
+++ javatests/artifacts/dagger/transitive-annotation-app/library1/src/main/java/library1/FooBase.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2021 The Dagger Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package library1;
+
+import static library2.MyTransitiveAnnotation.VALUE;
+
+import javax.inject.Inject;
+import library2.MyTransitiveAnnotation;
+
+/** A baseclass for {@link Foo}. */
+// @MyTransitiveAnnotation(VALUE): Not supported on base-types yet.
+public class FooBase {
+  @MyTransitiveAnnotation(VALUE)
+  int baseNonDaggerField;
+
+  // @MyTransitiveAnnotation(VALUE): Not supported on inject-method parameters yet.
+  @Inject int baseDaggerField;
+
+  @MyTransitiveAnnotation(VALUE)
+  FooBase(@MyTransitiveAnnotation(VALUE) String str) {}
+
+  // @MyTransitiveAnnotation(VALUE): Not supported on inject-constructors yet.
+  @Inject
+  FooBase(
+      // @MyTransitiveAnnotation(VALUE): Not supported on inject-constructor parameters yet.
+      int i) {}
+
+  @MyTransitiveAnnotation(VALUE)
+  void baseNonDaggerMethod(@MyTransitiveAnnotation(VALUE) int i) {}
+
+  // @MyTransitiveAnnotation(VALUE): Not supported on inject-method yet.
+  @Inject
+  void baseDaggerMethod(
+      // @MyTransitiveAnnotation(VALUE): Not supported on inject-method parameters yet.
+      int i) {}
+}
