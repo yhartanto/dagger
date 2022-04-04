c38550e7dd1027710c721e1a482356622fdd5154
Brad Corso <bcorso@google.com>
2022-04-04 16:16:29 -0700

    Dagger cleanup: remove unnecessary `checkNotNull` and unused inject parameter.
    RELNOTES=N/A
    PiperOrigin-RevId: 439436896

--- java/dagger/internal/codegen/writing/MembersInjectionMethods.java
+++ java/dagger/internal/codegen/writing/MembersInjectionMethods.java
@@ -16,7 +16,6 @@
 
 package dagger.internal.codegen.writing;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 import static com.squareup.javapoet.MethodSpec.methodBuilder;
 import static dagger.internal.codegen.base.Util.reentrantComputeIfAbsent;
@@ -71,11 +70,11 @@ final class MembersInjectionMethods {
       DaggerElements elements,
       DaggerTypes types,
       KotlinMetadataUtil metadataUtil) {
-    this.componentImplementation = checkNotNull(componentImplementation);
-    this.bindingExpressions = checkNotNull(bindingExpressions);
-    this.graph = checkNotNull(graph);
-    this.elements = checkNotNull(elements);
-    this.types = checkNotNull(types);
+    this.componentImplementation = componentImplementation;
+    this.bindingExpressions = bindingExpressions;
+    this.graph = graph;
+    this.elements = elements;
+    this.types = types;
     this.metadataUtil = metadataUtil;
   }
 
