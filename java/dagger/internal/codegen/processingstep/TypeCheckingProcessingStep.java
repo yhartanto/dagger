4048f51323f400e24ec666153d62f1dc70ff5385
Brad Corso <bcorso@google.com>
2022-08-12 10:49:27 -0700

    Roll forward of https://github.com/google/dagger/commit/1d74d1fd394fe570ca3dcf7f1eb84caf391dca8f
    RELNOTES=N/A
    PiperOrigin-RevId: 467245346

--- java/dagger/internal/codegen/processingstep/TypeCheckingProcessingStep.java
+++ java/dagger/internal/codegen/processingstep/TypeCheckingProcessingStep.java
@@ -75,7 +75,9 @@ abstract class TypeCheckingProcessingStep<E extends XElement> implements XProces
                 // TODO(b/201479062): It's inefficient to require validation of the entire enclosing
                 //  type, we should try to remove this and handle any additional validation into the
                 //  steps that need it.
-                superficialValidator.throwIfNearestEnclosingTypeNotValid(element);
+                if (requiresPreValidation()) {
+                  superficialValidator.throwIfNearestEnclosingTypeNotValid(element);
+                }
                 process((E) element, annotations);
               } catch (TypeNotPresentException e) {
                 // TODO(bcorso): We should be able to remove this once we replace all calls to
@@ -95,6 +97,15 @@ abstract class TypeCheckingProcessingStep<E extends XElement> implements XProces
               }
             });
     return deferredElements.build();
+  }
+
+  /**
+   * Returns {@code true} if this processing step requires pre-validation of the annotated element's
+   * nearest enclosing type element.
+   */
+  // TODO(bcorso): Once all processing steps handle their own validation we can remove this.
+  protected boolean requiresPreValidation() {
+    return true;
   }
 
   @Override
