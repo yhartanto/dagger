f67bdfc28ce1b7719bf59442120398aaf6f3df25
Nick Glorioso <glorioso@google.com>
2022-10-25 15:46:13 -0700

    Internal Change
    RELNOTES=n/a
    PiperOrigin-RevId: 483796087

--- java/dagger/internal/Preconditions.java
+++ java/dagger/internal/Preconditions.java
@@ -16,6 +16,7 @@
 
 package dagger.internal;
 
+
 /**
  * An adaptation of Guava's {@code com.google.common.base.Preconditions} that is specially tailored
  * to support checks applied in Dagger's generated code.
