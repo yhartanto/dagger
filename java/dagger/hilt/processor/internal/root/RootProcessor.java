6e293b5a641ee2f05367373caee6997cc3cb4f85
Kuan-Ying Chou <kuanyingchou@google.com>
2023-06-15 02:25:40 -0700

    Migrate RootProcessor to XProcessingStep
    RELNOTES=N/A
    PiperOrigin-RevId: 540515461

--- java/dagger/hilt/processor/internal/root/RootProcessor.java
+++ java/dagger/hilt/processor/internal/root/RootProcessor.java
@@ -16,197 +16,31 @@
 
 package dagger.hilt.processor.internal.root;
 
-import static com.google.common.base.Preconditions.checkState;
-import static dagger.hilt.processor.internal.HiltCompilerOptions.isCrossCompilationRootValidationDisabled;
-import static dagger.hilt.processor.internal.HiltCompilerOptions.isSharedTestComponentsEnabled;
 import static dagger.hilt.processor.internal.HiltCompilerOptions.useAggregatingRootProcessor;
-import static dagger.internal.codegen.extension.DaggerStreams.toImmutableList;
-import static dagger.internal.codegen.extension.DaggerStreams.toImmutableSet;
 import static net.ltgt.gradle.incap.IncrementalAnnotationProcessorType.AGGREGATING;
 import static net.ltgt.gradle.incap.IncrementalAnnotationProcessorType.DYNAMIC;
 import static net.ltgt.gradle.incap.IncrementalAnnotationProcessorType.ISOLATING;
 
-import androidx.room.compiler.processing.XElement;
-import androidx.room.compiler.processing.XFiler.Mode;
-import androidx.room.compiler.processing.XRoundEnv;
-import androidx.room.compiler.processing.XTypeElement;
 import com.google.auto.service.AutoService;
 import com.google.common.collect.ImmutableSet;
-import dagger.hilt.processor.internal.BadInputException;
-import dagger.hilt.processor.internal.BaseProcessor;
-import dagger.hilt.processor.internal.aggregateddeps.AggregatedDepsMetadata;
-import dagger.hilt.processor.internal.aliasof.AliasOfPropagatedDataMetadata;
-import dagger.hilt.processor.internal.definecomponent.DefineComponentClassesMetadata;
-import dagger.hilt.processor.internal.earlyentrypoint.AggregatedEarlyEntryPointMetadata;
-import dagger.hilt.processor.internal.generatesrootinput.GeneratesRootInputs;
-import dagger.hilt.processor.internal.root.ir.AggregatedDepsIr;
-import dagger.hilt.processor.internal.root.ir.AggregatedEarlyEntryPointIr;
-import dagger.hilt.processor.internal.root.ir.AggregatedRootIr;
-import dagger.hilt.processor.internal.root.ir.AggregatedRootIrValidator;
-import dagger.hilt.processor.internal.root.ir.AggregatedUninstallModulesIr;
-import dagger.hilt.processor.internal.root.ir.AliasOfPropagatedDataIr;
-import dagger.hilt.processor.internal.root.ir.ComponentTreeDepsIr;
-import dagger.hilt.processor.internal.root.ir.ComponentTreeDepsIrCreator;
-import dagger.hilt.processor.internal.root.ir.DefineComponentClassesIr;
-import dagger.hilt.processor.internal.root.ir.InvalidRootsException;
-import dagger.hilt.processor.internal.root.ir.ProcessedRootSentinelIr;
-import dagger.hilt.processor.internal.uninstallmodules.AggregatedUninstallModulesMetadata;
-import dagger.internal.codegen.xprocessing.XElements;
-import java.util.Arrays;
-import java.util.Set;
-import javax.annotation.processing.ProcessingEnvironment;
+import dagger.hilt.processor.internal.BaseProcessingStep;
+import dagger.hilt.processor.internal.JavacBaseProcessingStepProcessor;
 import javax.annotation.processing.Processor;
 import net.ltgt.gradle.incap.IncrementalAnnotationProcessor;
 
 /** Processor that outputs dagger components based on transitive build deps. */
 @IncrementalAnnotationProcessor(DYNAMIC)
 @AutoService(Processor.class)
-public final class RootProcessor extends BaseProcessor {
-
-  private boolean processed;
-  private GeneratesRootInputs generatesRootInputs;
-
+public final class RootProcessor extends JavacBaseProcessingStepProcessor {
   @Override
-  public synchronized void init(ProcessingEnvironment processingEnvironment) {
-    super.init(processingEnvironment);
-    generatesRootInputs = new GeneratesRootInputs(processingEnv());
+  protected BaseProcessingStep processingStep() {
+    return new RootProcessingStep(getXProcessingEnv());
   }
 
   @Override
   public ImmutableSet<String> additionalProcessingOptions() {
-    return useAggregatingRootProcessor(processingEnv())
+    return useAggregatingRootProcessor(getXProcessingEnv())
         ? ImmutableSet.of(AGGREGATING.getProcessorOption())
         : ImmutableSet.of(ISOLATING.getProcessorOption());
-  }
-
-  private Mode getMode() {
-    return useAggregatingRootProcessor(processingEnv()) ? Mode.Aggregating : Mode.Isolating;
-  }
-
-  @Override
-  public ImmutableSet<String> getSupportedAnnotationTypes() {
-    return ImmutableSet.<String>builder()
-        .addAll(
-            Arrays.stream(RootType.values())
-                .map(rootType -> rootType.className().toString())
-                .collect(toImmutableSet()))
-        .build();
-  }
-
-  @Override
-  public void processEach(XTypeElement annotation, XElement element) throws Exception {
-    XTypeElement rootElement = XElements.asTypeElement(element);
-    // TODO(bcorso): Move this logic into a separate isolating processor to avoid regenerating it
-    // for unrelated changes in Gradle.
-    RootType rootType = RootType.of(rootElement);
-    if (rootType.isTestRoot()) {
-      new TestInjectorGenerator(processingEnv(), TestRootMetadata.of(processingEnv(), rootElement))
-          .generate();
-    }
-
-    XTypeElement originatingRootElement =
-        Root.create(rootElement, processingEnv()).originatingRootElement();
-    new AggregatedRootGenerator(rootElement, originatingRootElement, annotation).generate();
-  }
-
-  @Override
-  public void postRoundProcess(XRoundEnv roundEnv) throws Exception {
-    if (!useAggregatingRootProcessor(processingEnv())) {
-      return;
-    }
-    ImmutableSet<XElement> newElements =
-        generatesRootInputs.getElementsToWaitFor(roundEnv).stream().collect(toImmutableSet());
-    if (processed) {
-      checkState(
-          newElements.isEmpty(),
-          "Found extra modules after compilation: %s\n"
-              + "(If you are adding an annotation processor that generates root input for hilt, "
-              + "the annotation must be annotated with @dagger.hilt.GeneratesRootInput.\n)",
-          newElements.stream().map(XElements::toStableString).collect(toImmutableList()));
-    } else if (newElements.isEmpty()) {
-      processed = true;
-
-      ImmutableSet<AggregatedRootIr> rootsToProcess = rootsToProcess();
-      if (rootsToProcess.isEmpty()) {
-        return;
-      }
-      // Generate an @ComponentTreeDeps for each unique component tree.
-      ComponentTreeDepsGenerator componentTreeDepsGenerator =
-          new ComponentTreeDepsGenerator(processingEnv(), getMode());
-      for (ComponentTreeDepsMetadata metadata : componentTreeDepsMetadatas(rootsToProcess)) {
-        componentTreeDepsGenerator.generate(metadata);
-      }
-
-      // Generate a sentinel for all processed roots.
-      for (AggregatedRootIr ir : rootsToProcess) {
-        XTypeElement rootElement = processingEnv().requireTypeElement(ir.getRoot().canonicalName());
-        new ProcessedRootSentinelGenerator(rootElement, getMode()).generate();
-      }
-    }
-  }
-
-  private ImmutableSet<AggregatedRootIr> rootsToProcess() {
-    ImmutableSet<ProcessedRootSentinelIr> processedRoots =
-        ProcessedRootSentinelMetadata.from(processingEnv()).stream()
-            .map(ProcessedRootSentinelMetadata::toIr)
-            .collect(toImmutableSet());
-    ImmutableSet<AggregatedRootIr> aggregatedRoots =
-        AggregatedRootMetadata.from(processingEnv()).stream()
-            .map(AggregatedRootMetadata::toIr)
-            .collect(toImmutableSet());
-
-    boolean isCrossCompilationRootValidationDisabled =
-        isCrossCompilationRootValidationDisabled(
-            aggregatedRoots.stream()
-                .map(ir -> processingEnv().requireTypeElement(ir.getRoot().canonicalName()))
-                .collect(toImmutableSet()),
-            processingEnv());
-    try {
-      return ImmutableSet.copyOf(
-          AggregatedRootIrValidator.rootsToProcess(
-              isCrossCompilationRootValidationDisabled, processedRoots, aggregatedRoots));
-    } catch (InvalidRootsException ex) {
-      throw new BadInputException(ex.getMessage());
-    }
-  }
-
-  private ImmutableSet<ComponentTreeDepsMetadata> componentTreeDepsMetadatas(
-      ImmutableSet<AggregatedRootIr> aggregatedRoots) {
-    ImmutableSet<DefineComponentClassesIr> defineComponentDeps =
-        DefineComponentClassesMetadata.from(processingEnv()).stream()
-            .map(DefineComponentClassesMetadata::toIr)
-            .collect(toImmutableSet());
-    ImmutableSet<AliasOfPropagatedDataIr> aliasOfDeps =
-        AliasOfPropagatedDataMetadata.from(processingEnv()).stream()
-            .map(AliasOfPropagatedDataMetadata::toIr)
-            .collect(toImmutableSet());
-    ImmutableSet<AggregatedDepsIr> aggregatedDeps =
-        AggregatedDepsMetadata.from(processingEnv()).stream()
-            .map(AggregatedDepsMetadata::toIr)
-            .collect(toImmutableSet());
-    ImmutableSet<AggregatedUninstallModulesIr> aggregatedUninstallModulesDeps =
-        AggregatedUninstallModulesMetadata.from(processingEnv()).stream()
-            .map(AggregatedUninstallModulesMetadata::toIr)
-            .collect(toImmutableSet());
-    ImmutableSet<AggregatedEarlyEntryPointIr> aggregatedEarlyEntryPointDeps =
-        AggregatedEarlyEntryPointMetadata.from(processingEnv()).stream()
-            .map(AggregatedEarlyEntryPointMetadata::toIr)
-            .collect(toImmutableSet());
-
-    // We should be guaranteed that there are no mixed roots, so check if this is prod or test.
-    boolean isTest = aggregatedRoots.stream().anyMatch(AggregatedRootIr::isTestRoot);
-    Set<ComponentTreeDepsIr> componentTreeDeps =
-        ComponentTreeDepsIrCreator.components(
-            isTest,
-            isSharedTestComponentsEnabled(processingEnv()),
-            aggregatedRoots,
-            defineComponentDeps,
-            aliasOfDeps,
-            aggregatedDeps,
-            aggregatedUninstallModulesDeps,
-            aggregatedEarlyEntryPointDeps);
-    return componentTreeDeps.stream()
-        .map(it -> ComponentTreeDepsMetadata.from(it, processingEnv()))
-        .collect(toImmutableSet());
   }
 }
